6bf27d0a026286763fc98c87ca78b78f3788153b
Allyson Finck <allyson.finck@gmail.com>
2018-05-10 12:10:26 -0400

    application results display data

--- public/js/applicationlist.js
+++ public/js/applicationlist.js
@@ -2,36 +2,55 @@ class ApplicationList extends React.Component {
   constructor(props){
     super(props)
     this.state={
-      applicationShowIsVisible: false
+      applicationShowIsVisible: false,
+      applications: []
     }
     this.toggleState = this.toggleState.bind(this)
+  }
+  componentDidMount(){
+    this.getApplications();
   }
   toggleState(st){
     this.setState({[st]: !this.state[st]})
   }
+  getApplications(){
+    fetch('/applications')
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          applications: data
+        })
+      }).catch(error => console.log(error))
+  }
+
   render () {
     return (
       <div>
         <table className='table'>
           <tbody>
-            <tr>
-              <td>
-                <h3>May 4, 2018</h3>
-              </td>
-              <td>
-                <h3>IBM</h3>
-              </td>
-              <td>
-                <h3>Front End Developer</h3>
-              </td>
-              <td>
-                <button type='button' className='btn btn-info' onClick={()=>this.toggleState('applicationShowIsVisible')}>Edit</button>
-              </td>
-              <td>
-                <button type='button' className='btn btn-danger'>Delete</button>
-              </td>
-            </tr>
-          </tbody>          
+          {this.state.applications.map((application, index) => {
+            return (
+              <tr>
+                <td>
+                  <h3>{application.date_applied}</h3>
+                </td>
+                <td>
+                  <h3>{application.company}</h3>
+                </td>
+                <td>
+                  <h3>{application.job_title}</h3>
+                </td>
+                <td>
+                  <button type='button' className='btn btn-info' onClick={()=>this.toggleState('applicationShowIsVisible')}>Edit</button>
+                </td>
+                <td>
+                  <button type='button' className='btn btn-danger'>Delete</button>
+                </td>
+              </tr>
+            )
+          })}
+
+          </tbody>
         </table>
         {this.state.applicationShowIsVisible ? <ApplicationView /> : ''}
       </div>
